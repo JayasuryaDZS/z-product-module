836ad2d33d516546d8bfb0add0459b90f0774eb8
Zylen-Guru <gurunathan.s@zylensolutions.com>
2024-04-08 11:58:59 +0530

    latest

---
+++ src/pages/Practices/AddNewPractice.tsx
@@ -0,0 +1,97 @@
+const AddNewPractice = () => {
+    return (
+        <div className="AddNewPractice">
+            <form className="container create-form mt-3">
+                <div className="row">
+                    <div className="col-12 col-md-4 mt-2">
+                        <h6>Practice Details</h6>
+                        <label>Practice Logo</label>
+                        <input
+                            placeholder="Super Admin Name"
+                            type="text"
+                            className="form-control"
+                        />
+                    </div>
+                </div>
+                <hr />
+                <div className="row" >
+
+                    <div className="col-12 col-md-4 mt-1">
+                        <h6>Super Admin Details</h6>
+                        <label>Super admin name</label>
+                        <input
+                            placeholder="Super Admin Name"
+                            type="text"
+                            className="form-control"
+                        />
+                    </div>
+                    <div className="row" >
+
+                        <div className="col-12 col-md-4 mt-2">
+
+                            <label>Super admin name</label>
+                            <input
+                                placeholder="Super Admin Name"
+                                type="text"
+                                className="form-control"
+                            />
+                        </div>
+                        <div className="col-12 col-md-4 mt-2">
+
+                            <label>Super admin name</label>
+                            <input
+                                placeholder="Super Admin Name"
+                                type="text"
+                                className="form-control"
+                            />
+                        </div>
+                    </div>
+                </div>
+                <hr />
+                <div className="row" >
+                    <div className="col-12 col-md-4 mt-2">
+                        <h6>Practice Address Details</h6>
+                        <label>Super admin name</label>
+                        <input
+                            placeholder="Super Admin Name"
+                            type="text"
+                            className="form-control"
+                        />
+                    </div>
+                    <div className="col-12 col-md-4 mt-2">
+
+                        <label>Super admin name</label>
+                        <input
+                            placeholder="Super Admin Name"
+                            type="text"
+                            className="form-control"
+                        />
+                    </div>
+                    <div className="row" >
+
+                        <div className="col-12 col-md-4 mt-2">
+
+                            <label>Super admin name</label>
+                            <input
+                                placeholder="Super Admin Name"
+                                type="text"
+                                className="form-control"
+                            />
+                        </div>
+                        <div className="col-12 col-md-4 mt-2">
+
+                            <label>Super admin name</label>
+                            <input
+                                placeholder="Super Admin Name"
+                                type="text"
+                                className="form-control"
+                            />
+                        </div>
+                    </div>
+                </div>
+            </form >
+        </div >
+    );
+}
+
+export default AddNewPractice;
--- src/pages/Practices/ParcticeCreation.tsx
+++ src/pages/Practices/ParcticeCreation.tsx
@@ -48,11 +48,11 @@
 //                             className={`circle ${selectedCircle === circleNumber ? 'selected' : ''}`}
 //                             onClick={() => handleCircleClick(circleNumber)}
 //                         >
-                         
+
 //                             {circleNumber}
-                            
+
 //                         </div>
-                        
+
 //                     </React.Fragment>
 //                 ))}
 //             </div>
@@ -80,9 +80,90 @@
 // export default PracticeCreation;
 
 
+// import React, { useState } from "react";
+// import { useNavigate } from "react-router-dom";
+// import Button from "../../Components/Button";
+
+// const PracticeCreation = () => {
+//     const navigate = useNavigate();
+//     const [selectedCircle, setSelectedCircle] = useState(1);
+
+//     const handleNextClick = () => {
+//         if (selectedCircle < 4) {
+//             setSelectedCircle(selectedCircle + 1);
+//         } else {
+//             navigate("/practices");
+//         }
+//     };
+
+//     const handleCircleClick = (circleNumber:any) => {
+//         setSelectedCircle(circleNumber);
+//     };
+
+//     const titles = [
+//         "Add New Practice",
+//         "Subscription",
+//         "Delete",
+//         "Edit"
+//     ];
+
+//     const renderAdditionalContent = () => {
+//         switch (selectedCircle) {
+//             case 1:
+//                 return <div></div>;
+//             case 2:
+//                 return <div>Some other content for circle 2</div>;
+//             case 3:
+//                 return <div>Some other content for circle 3</div>;
+//             case 4:
+//                 return <div>Some other content for circle 4</div>;
+//             default:
+//                 return null;
+//         }
+//     };
+
+//     return (
+//         <div className="d-flex flex-column Practice">
+//             <div className="circleContainer flex-column">
+//                 {[1, 2, 3, 4].map(circleNumber => (
+//                     <React.Fragment key={circleNumber}>
+//                         {circleNumber > 1 && <div className={`line ${selectedCircle >= circleNumber ? 'line-selected' : ''}`} />}
+//                         <div
+//                             className={`circle ${selectedCircle === circleNumber ? 'selected' : ''}`}
+//                             onClick={() => handleCircleClick(circleNumber)}
+//                         >
+//                             {circleNumber}
+//                         </div>
+//                     </React.Fragment>
+//                 ))}
+//             </div>
+//             <div className='position-fixed headingPrac'>
+//                 <div className="heading1">
+//                     <p className='fs-4 fw-bolder'>{titles[selectedCircle - 1]}</p>
+//                     <div className='d-flex'>
+//                         <Button btntype='outlined' className='me-3' onClick={() => {
+//                             console.log("Button clicked");
+//                             navigate("/practices");
+//                         }}>Cancel</Button>
+//                         <Button btntype='filled' onClick={handleNextClick}>
+//                             {selectedCircle < 4 ? "Next" : "Submit"}
+//                         </Button>
+//                     </div>
+//                 </div>
+//                 <div className="additionalContent">
+//                     {renderAdditionalContent()}
+//                 </div>
+//             </div>
+//         </div>
+//     );
+// };
+
+// export default PracticeCreation;
+
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import Button from "../../Components/Button";
+import Button from "../../Components/Button"; // Import the AddNewPractice component
+import AddNewPractice from "./AddNewPractice";
 
 const PracticeCreation = () => {
     const navigate = useNavigate();
@@ -96,7 +177,7 @@ const PracticeCreation = () => {
         }
     };
 
-    const handleCircleClick = (circleNumber:any) => {
+    const handleCircleClick = (circleNumber: any) => {
         setSelectedCircle(circleNumber);
     };
 
@@ -110,7 +191,7 @@ const PracticeCreation = () => {
     const renderAdditionalContent = () => {
         switch (selectedCircle) {
             case 1:
-                return <div>Guru</div>;
+                return <AddNewPractice />;
             case 2:
                 return <div>Some other content for circle 2</div>;
             case 3:
